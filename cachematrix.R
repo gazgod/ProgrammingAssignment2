2fea13ee9719f47dd35504b2000e762bc2684049
gazgod <gaz.god@gmail.com>
2014-07-27 22:41:13 +0100

    Commented

--- cachematrix.R
+++ cachematrix.R
@@ -1,35 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera R programming Assignment 2 CacheMatrix
+## 16/07/2014
+## By GazGod
 
-## Write a short comment describing this function
+## An R Function that can cache potentially time consuming computations
+## in this case cache the inverse of a matrix so the cached value can be returned instead of recalculation.
+
+
+## create a special matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
-        set <- function(y) {
-                x <<- y
-                m <<- NULL
+        m <- NULL                              # Initialize inverse matrix as NULL
+        set <- function(y) {                   # Set matrix new matrix value
+                x <<- y                        # Get value of y from parent environments
+                m <<- NULL                     # Reset inverse matrix when input matrix is changed
         }
-        get <- function() x
-        setsolve <- function(solve) m <<- solve
-        getsolve <- function() m
-        list(set = set, 
+        get <- function() x                    # get the matrix value
+        setsolve <- function(solve) m <<- solve  # set the matrix inversion
+        getsolve <- function() m               # get the matrix inversion
+        list(set = set,                        # list of functions
              get = get,
              setsolve = setsolve,
              getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## Function to compute the matrix inversion
+## If already calculated return the cached inversion
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         m <- x$getsolve()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
+        if(!is.null(m)) {                      # if inversion already cached return it
+                message("getting cached data") 
+                return(m)                      # return cached inversion
         }
-        data <- x$get()
-        m <- solve(data)
-        x$setsolve(m)
-        m
+        data <- x$get()                        # assign data equal to the source matrix
+        m <- solve(data)                       # calculate the inversion
+        x$setsolve(m)                          # cache the inversion
+        m                                      # return calculated inversion
 }
